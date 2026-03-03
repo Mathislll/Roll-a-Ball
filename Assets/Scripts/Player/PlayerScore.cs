922f6ac630e5517d503dac84dcecf8c851b46af8
Mathis Chevry <mathischevry@gmail.com>
2026-03-03 12:38:29 +0100

    Add PlayerScore script

---
+++ Assets/Scripts/Player/PlayerScore.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class PlayerScore : MonoBehaviour
+{
+    public int score = 0;
+
+    public void AddScore(int points)
+    {
+        score += points;
+        Debug.Log("Score: " + score);
+    }
+
+}
